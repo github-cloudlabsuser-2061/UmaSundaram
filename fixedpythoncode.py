2d30c398873d0cfcf110af858bcde1804394498d
github-cloudlabsuser-2061 <github_cloudlabsuser_2061@cloudlabsaiuser.com>
2024-06-18 05:51:05 +0000

    Copilot fourth commit

---
+++ fixedpythoncode.py
@@ -0,0 +1,13 @@
+# Intentionally flawed Python program
+
+# importing modules
+import itertools, random
+
+# make a deck of cards
+deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club']))
+
+# shuffle the cards
+random.shuffle(deck)
+
+# draw five cards
+drawn_cards = deck[:5]
